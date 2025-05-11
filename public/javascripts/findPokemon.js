e1b600200212e322c14f650d7ffa269dae4c0e84
ksor029 <a1809029@adelaide.edu.au>
2025-05-11 07:35:39 +0000

    functions to calculate individual pokemon's type defenses added

--- public/javascripts/findPokemon.js
+++ public/javascripts/findPokemon.js
@@ -481,6 +481,81 @@ const typeInteraction = {
     }
 };
 
+function calculateTypeInteraction(pokemonType) {
+    let typeDefenses = {
+        normal: 1,
+        fighting: 1,
+        flying: 1,
+        poison: 1,
+        ground: 1,
+        rock: 1,
+        bug: 1,
+        ghost: 1,
+        steel: 1,
+        fire: 1,
+        water: 1,
+        grass: 1,
+        electric: 1,
+        psychic: 1,
+        ice: 1,
+        dragon: 1,
+        dark: 1,
+        fairy: 1,
+        stellar: 1,
+        unknown: 1
+    };
+
+    if (pokemonType.length > 1) {
+        let type0 = typeInteraction[pokemonType[0]].defenseMultiplier;
+        let type1 = typeInteraction[pokemonType[1]].defenseMultiplier;
+
+        for (let key in typeDefenses){
+            if (typeof typeDefenses[key] == 'number'){
+            typeDefenses[key] = type0[key] * type1[key];
+            }
+        }
+    } else {
+        typeDefenses = typeInteraction[pokemonType[0]].defenseMultiplier;
+    }
+
+    return typeDefenses;
+}
+
+function determineWeaknesses(defenses){
+    let weakArray = [];
+        for (let key in defenses){
+            if (typeof defenses[key] == 'number'){
+                if (defenses[key] > 1) {
+                    weakArray.push(key);
+                }
+            }
+        }
+    return weakArray;
+}
+
+function determineResistances(defenses){
+    let resistArray = [];
+        for (let key in defenses){
+            if (typeof defenses[key] == 'number'){
+                if (defenses[key] < 1 && defenses[key] > 0) {
+                    resistArray.push(key);
+                }
+            }
+        }
+    return resistArray;
+}
+
+function determineImmunities(defenses){
+    let immuneArray = [];
+        for (let key in defenses){
+            if (typeof defenses[key] == 'number'){
+                if (defenses[key] === 0) {
+                    immuneArray.push(key);
+                }
+            }
+        }
+    return immuneArray;
+}
 
 function findPokemon() {
     const pokemonName = document.getElementById('pokemon-name-input').value.toLowerCase();
@@ -499,18 +574,62 @@ function findPokemon() {
                 pokemonType.push(pokemonTypeRaw[i].type.name);
             }
 
+            /* THESE PARTS AFTER AREN'T RELATED TO THE API SEARCH FUNCTION */
+            const typeDefenses = calculateTypeInteraction(pokemonType);
+            const defWeaknesses = determineWeaknesses(typeDefenses);
+            const defResistances = determineResistances(typeDefenses);
+            const defImmunities = determineImmunities(typeDefenses);
+
             /* poketest.html specific */
             const resultDiv = document.getElementById('results');
             resultDiv.style.display = "block";
 
             const typeP = document.getElementById('paragraph-1');
             typeP.innerText = pokemonName + "'s typing:";
+            const listType = document.getElementById('typing');
+            listType.innerHTML = ""; // clear the list element
             for (let i = 0; i < pokemonTypeRaw.length ; i++){
-                const node = document.getElementById('type-' + i);
-                node.style.display = "block";
+                const node = document.createElement('li');
                 node.innerText = pokemonType[i];
+                listType.appendChild(node);
             }
 
+            /* TYPE INTERACTIONS */
+            const p2 = document.getElementById('paragraph-2');
+            p2.innerText = pokemonName + " is weak against:";
+            const listWeak = document.getElementById('weaknesses');
+            listWeak.innerHTML = ""; // clear the list element
+            for (let i = 0; i < defWeaknesses.length; i++){
+                const node = document.createElement('li');
+                node.innerText = defWeaknesses[i];
+                listWeak.appendChild(node);
+            }
+
+            const p3 = document.getElementById('paragraph-3');
+            p3.innerText = pokemonName + " is resistant to:";
+            const listResist = document.getElementById('resistances');
+            listResist.innerHTML = ""; // clear the list element
+            for (let i = 0; i < defResistances.length; i++){
+                const node = document.createElement('li');
+                node.innerText = defResistances[i];
+                listResist.appendChild(node);
+            }
+
+            const p4 = document.getElementById('paragraph-4');
+            const listImmune = document.getElementById('immunities');
+            listImmune.innerHTML = ""; // clear the list element
+            if (defImmunities.length > 0){
+                p4.innerText = pokemonName + " is immune to:";
+                for (let i = 0; i < defImmunities.length; i++){
+                    const node = document.createElement('li');
+                    node.innerText = defImmunities[i];
+                    listImmune.appendChild(node);
+                }
+            } else {
+                p4.innerText = pokemonName + " has not immunities.";
+            }
+
+            /* ART */
             const officialArtElement = document.getElementById('pokemon-official-art');
             officialArtElement.src = pokemonOfficialArt;
 
